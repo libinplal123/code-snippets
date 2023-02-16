c5f2f5b9133261932b5db280d1422a4f3cd7dc53
libinplal123 <114125415+libinplal123@users.noreply.github.com>
2023-02-16 09:50:55 +0530

    Update javascript_views.py

--- javascript_views.py
+++ javascript_views.py
@@ -2,8 +2,6 @@ Step 6: Update the Django view to handle the form data and create a new user.
 
 Update the register view in views.py to handle the form data and create a new user. Once the user is created, authenticate and log them in, and return a JSON response indicating success.
 
-python
-Copy code
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
